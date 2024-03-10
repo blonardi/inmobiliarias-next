fa1937d703f2c02ee0e973d543f2e1e98560aca7
blonardi <lonardibautista@gmail.com>
2024-03-10 20:00:07 -0300

    Fix route.ts fetch

--- src/app/api/casas/route.ts
+++ src/app/api/casas/route.ts
@@ -1,8 +1,8 @@
-import { NextResponse } from "next/server"
+//import { NextResponse } from "next/server"
 
-export async function GET(){
+export async function getHouses(){
 	const response = await fetch('http://localhost:3001/api/houses')
 	const data = await response.json()
 	//console.log(data)
-	return NextResponse.json(data)
+	return (data)
 }
